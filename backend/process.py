c4ad3b0f5e132e2f9ac1b7179b044fea029246b6
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-19 01:52:40 +0600

    Added hashtable of commands

--- backend/process.py
+++ backend/process.py
@@ -1,11 +1,21 @@
 import speech_recognition as sr
+import json
+import webbrowser
+def open_youtube():
+    webbrowser.open("https://www.youtube.com")
+commands = {
+    "open youtube": ["Opening youtube...", open_youtube]
+}
 def listen():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source, timeout=3)
         try:
             text = r.recognize_google(audio)
-            print(text)
+            text = text.lower()
+            json_res = {"text": text, "assistant_reply": commands.get(text, "Not found")[0]}
+            commands.get(text)[1]()
+            print(json.dumps(json_res))
         except:
             print("Error")
 
