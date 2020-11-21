3f2e44bdfcb8751a716a84869a467966929be523
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-22 00:10:18 +0600

    Updated with recent unread messages command

--- backend/services/commands.py
+++ backend/services/commands.py
@@ -2,9 +2,10 @@ from .actions import Actions
 class Commands:
     def __init__(self):
         self.commands = {
-                "open youtube": ["Opening youtube...", Actions.open_youtube],
-                "open explorer": ["Opening explorer...", Actions.open_explorer],
-                "open facebook": ["Opening facebook...", Actions.open_facebook]
+                "open youtube": ["Opening youtube...", Actions.open_youtube, -1],
+                "open explorer": ["Opening explorer...", Actions.open_explorer, -1],
+                "open facebook": ["Opening facebook...", Actions.open_facebook, -1],
+                "recent unread messages": ["Checking messages...", lambda x: None, 0]
             }
     def get_commands(self):
         return self.commands
