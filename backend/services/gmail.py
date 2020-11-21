ff922f9355c088fe5663d626618ff09cc43f2e58
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-22 00:14:27 +0600

    Added for accessing gmail messages through gmail api

---
+++ backend/services/gmail.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    # get messages 
+    results = service.users().messages().list(userId='me', labelIds='UNREAD').execute()
+    messages = results.get('messages', [])
+    if not labels:
+        print('No labels found.')
+    else:
+        # print('Labels:')
+        # for label in labels:
+        #     print(label['name'])
+        msg_list = []
+        for message in messages[:5]:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            # print(msg['snippet'])
+            msg_list.append(msg['snippet'])
+        return msg_list
