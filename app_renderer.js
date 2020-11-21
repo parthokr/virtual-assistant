ae18353dfda18d5ecc49786c749476c858590001
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-21 14:15:36 +0600

    Updated with textCommand()

--- app_renderer.js
+++ app_renderer.js
@@ -1,3 +1,4 @@
+const toastr = require("toastr");
 const mic = document.getElementById("mic");
 const addNewCommand = (cmd) => {
     let newCmd = document.createElement("div");
@@ -5,7 +6,6 @@ const addNewCommand = (cmd) => {
                 <div class="d-flex justify-content-end mb-4">\
                             <div class="msg_cotainer_send">\
                                 ${cmd}\
-                                <span class="msg_time_send">8:55 AM, Today</span>\
                             </div>\
                             <div class="img_cont_msg">\
                                 <img src="assets/img/user.svg" class="rounded-circle user_img_msg">\
@@ -25,14 +25,13 @@ const addAssistantReply = (cmd) => {
                         </div>
                         <div class="msg_cotainer">
                             ${cmd}
-                            <span class="msg_time">8:40 AM, Today</span>
                         </div>
                     </div>`;
     let commandsCard = document.getElementById("commands");
     commandsCard.appendChild(newCmd);
     commandsCard.scrollTop =  commandsCard.scrollHeight;
 }
-mic.addEventListener("click", () => {
+mic.addEventListener("click", (event) => {
     console.log("Listening");
     let listeningAudio = new Audio("assets/audio/listening.mp3");
     listeningAudio.play();
@@ -41,38 +40,58 @@ mic.addEventListener("click", () => {
     {
       _mic.classList.remove("fa-microphone");
       _mic.classList.add("fa-stop-circle");
-      document.querySelector("textarea[name='command']").placeholder="Listening...";
-    }else{
-    _mic.classList.remove("fa-stop-circle");
-    _mic.classList.add("fa-microphone");  
-          document.querySelector("textarea[name='command']").placeholder = "Type your command...";
-    }
-        var python = require("child_process").spawn(
-          "./backend/venv/Scripts/python",
-          ["./backend/process.py",]
-        );
-        python.stdout.on("data", function (data) {
-          console.log("Python response: ", data.toString());
-          let res = JSON.parse(data.toString());
-            let doneAudio = new Audio("assets/audio/done.mp3");
-            doneAudio.play();
+      document.querySelector("input[name='command']").placeholder="Listening...";
+      var python = require("child_process").spawn("./backend/venv/Scripts/python", [
+        "./backend/run.py",
+      ]);
+      python.stdout.on("data", function (data) {
+        console.log("Python response: ", data.toString());
+        let res = JSON.parse(data.toString());
+        let doneAudio = new Audio("assets/audio/done.mp3");
+        doneAudio.play();
+        if (res["assistant_reply"] == "not_found") {
+          toastr.warning("Command not found");
+        }else{
           addNewCommand(res["text"]);
           addAssistantReply(res["assistant_reply"]);
-        const _mic = document.getElementById("_mic");
-        if (_mic.classList.contains("fa-microphone")) {
-        _mic.classList.remove("fa-microphone");
-        _mic.classList.add("fa-stop-circle");
-        document.querySelector("textarea[name='command']").placeholder = "Listening...";
-        } else {
-        _mic.classList.remove("fa-stop-circle");
-        _mic.classList.add("fa-microphone");
-        document.querySelector("textarea[name='command']").placeholder = "Type your command...";
         }
-        //   const response = JSON.parse(data.toString());
-        //   console.log(response);
-        //   if (response["signed_up"]) {
-        //     ipcRenderer.send("asynchronous-message", "successfullySignedup");
-        //   }
-          // result.textContent = data.toString("utf8");
-        });
+        const _mic = document.getElementById("_mic");
+        if (_mic.classList.contains("fa-stop-circle")) {
+          _mic.classList.remove("fa-stop-circle");
+          _mic.classList.add("fa-microphone");
+          document.querySelector("input[name='command']").placeholder =
+            "Type your command...";
+        }
+      });
+    }
+    else{
+      event.preventDefault();
+    }
+});
+const textCommand = (cmd) => {
+  console.log(cmd)
+    addNewCommand(cmd.join(" "));
+    var python = require("child_process").spawn(
+      "./backend/venv/Scripts/python",
+      ["./backend/run.py",...cmd]
+    );
+    python.stdout.on("data", function (data) {
+      console.log("Python response: ", data.toString());
+      let res = JSON.parse(data.toString());
+      let doneAudio = new Audio("assets/audio/done.mp3");
+      doneAudio.play();
+      if (res["assistant_reply"] == "not_found") {
+        toastr.warning("Command not found");
+      } else {
+        addAssistantReply(res["assistant_reply"]);
+      }
+    });
+}
+const command_input = document.querySelector('input[name="command"]');
+command_input.addEventListener("keypress", (event) => {
+  if(event.keyCode == 13)
+  {
+    textCommand(command_input.value.split(' '));
+    command_input.value = "";
+  }
 });
