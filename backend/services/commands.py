94e6191c5b7acc159a9dfc57200f78ea7ad2dc33
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-24 00:45:47 +0600

    Added open chrome command

--- backend/services/commands.py
+++ backend/services/commands.py
@@ -5,6 +5,8 @@ class Commands:
                 "open youtube": ["Opening youtube...", Actions.open_youtube, -1],
                 "open explorer": ["Opening explorer...", Actions.open_explorer, -1],
                 "open facebook": ["Opening facebook...", Actions.open_facebook, -1],
+                "open chrome": ["Opening chrome...", Actions.open_chrome, -1],
+
                 "messages": ["Checking messages...", lambda x: None, 0],
 
                 "my ip": ["Fetching your ip...", lambda x: None, 1],
