278cb0097ac04f13df36182cf81433228bd44b9e
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-23 23:21:37 +0600

    Added callback for covid update

--- backend/run_callback.py
+++ backend/run_callback.py
@@ -1,16 +1,30 @@
 from sys import argv
 import json
 from services.gmail import main
+from services.covid_update import main as covid_update
 import requests
 cb_id = int(argv[1])
 if(cb_id==0):
     msg_list = main()
     # print(msg_list)
-    msg_list_json = json.dumps({"data": msg_list, "head": "Messages"})
-    print(msg_list_json)
+    res_json = {"data":{}}
+    res_json["data"]["Message 1"] = msg_list[0]
+    res_json["data"]["Message 2"] = msg_list[1]
+    res_json["data"]["Message 3"] = msg_list[2]
+    res_json["data"]["Message 4"] = msg_list[3]
+    res_json["data"]["Message 5"] = msg_list[4]
+    print(json.dumps(res_json))
 elif(cb_id==1):
     req_json_res = json.loads(requests.get("https://api.myip.com/").text)
     res_json = {}
-    res_json["context"] = "Your IP"
-    res_json["data"] = [req_json_res["ip"]]
+    res_json["data"] = {"IP": req_json_res["ip"]}
+    print(json.dumps(res_json))
+elif(cb_id==2):
+    res = covid_update()[0]
+    res_json = {"data":{}}
+    res_json["data"]["Country"] = res["country"]
+    res_json["data"]["Confirmed"] = res["confirmed"]
+    res_json["data"]["Recovered"] = res["recovered"]
+    res_json["data"]["Deaths"] = res["deaths"]
+
     print(json.dumps(res_json))
