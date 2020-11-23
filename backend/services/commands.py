d7857baf4034f823237098887d53d7328f1ffbf4
ParthoKR <partho.rajvor2016@gmail.com>
2020-11-23 23:21:55 +0600

    Added covid update command

--- backend/services/commands.py
+++ backend/services/commands.py
@@ -6,7 +6,8 @@ class Commands:
                 "open explorer": ["Opening explorer...", Actions.open_explorer, -1],
                 "open facebook": ["Opening facebook...", Actions.open_facebook, -1],
                 "messages": ["Checking messages...", lambda x: None, 0],
-                "my ip": ["Fetching your ip...", lambda x: None, 1]
+                "my ip": ["Fetching your ip...", lambda x: None, 1],
+                "covid update": ["Fetching covid update...", lambda x: None, 2]
             }
     def get_commands(self):
         return self.commands
